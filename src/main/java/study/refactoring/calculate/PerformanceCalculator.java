bf1720709745141e254bea49f3ac315dbbb24b72
heechul <khc4190@naver.com>
2025-01-22 22:02:58 +0900

    조건부 로직 다형성으로 바꾸기

--- src/main/java/study/refactoring/calculate/PerformanceCalculator.java
+++ src/main/java/study/refactoring/calculate/PerformanceCalculator.java
@@ -36,22 +36,6 @@ public class PerformanceCalculator {
     }
 
     public int getAmount() {
-        int result;
-        switch (this.play.getType()) {
-            case "tragedy": {
-                throw new Error("오류 발생");
-            }
-            case "comedy": {
-                result = 30000;
-                if (this.performance.getAudience() > 20) {
-                    result += 10000 + 500 * (this.performance.getAudience() - 20);
-                }
-                result += 300 * this.performance.getAudience();
-            }
-            break;
-            default:
-                throw new Error(String.format("알 수 없는 장르: %s", this.play.getType()));
-        }
-        return result;
+        throw new Error("서브 클래스에서 처리하도록 설계되었습니다.");
     }
 }
