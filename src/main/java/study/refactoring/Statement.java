75fa30071cb8d47c0095b478e27010d216cc171b
heechul <khc4190@naver.com>
2025-01-22 21:38:42 +0900

    html 코드 추가

--- src/main/java/study/refactoring/Statement.java
+++ src/main/java/study/refactoring/Statement.java
@@ -14,10 +14,24 @@ public class Statement {
     private String renderPlainText(StatementData data) {
         StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", data.getCustomer()));
         for (PerformanceData performance : data.getPerformances()) {
-            result.append(String.format("  %s: %s (%s석)\n", performance.getPlay().getName(), usd(performance.getAmount()), performance.getAudience()));
+            result.append(String.format("  %s: %s (%d석)\n", performance.getPlay().getName(), usd(performance.getAmount()), performance.getAudience()));
         }
         result.append(String.format("총액: %s\n", usd(data.getTotalAmount())));
         result.append(String.format("적립 포인트: %s점\n", data.getTotalVolumeCredits()));
+        return result.toString();
+    }
+
+    private String renderHtml(StatementData data) {
+        StringBuilder result = new StringBuilder(String.format("<h1>청구 내역 (고객명: %s)</h1>\n", data.getCustomer()));
+        result.append("<table>\n");
+        result.append("<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>");
+        for (PerformanceData performance : data.getPerformances()) {
+            result.append(String.format("  <tr><td>%s</td><td>(%d석)</td>", performance.getPlay().getName(), performance.getAudience()));
+            result.append(String.format("<td>%s</td></tr>\n", usd(performance.getAmount())));
+        }
+        result.append("</table>\n");
+        result.append(String.format("<p>총액: <em>%s</em>점</p>\n", data.getTotalAmount()));
+        result.append(String.format("<p>적립 포인트: <em>%s</em>점</p>\n", data.getTotalVolumeCredits()));
         return result.toString();
     }
 
