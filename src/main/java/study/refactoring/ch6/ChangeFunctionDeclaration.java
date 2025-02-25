8f9f584a33093a8350d4ea8df449bbe17ed1db8e
heechul <khc4190@naver.com>
2025-02-25 16:07:35 +0900

    함수 선언 바꾸기 리팩터링 - 마이그레이션 절차

--- src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
+++ src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
@@ -2,7 +2,11 @@ package study.refactoring.ch6;
 
 public class ChangeFunctionDeclaration {
 
-	private static double circumference(double radius) {
+	private static double circum(double radius) {
+		return circumference(radius);
+	}
+
+	private static double circumference(double radius){
 		return 2 * Math.PI * radius;
 	}
 }
