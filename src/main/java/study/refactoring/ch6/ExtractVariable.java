b3d8e8e52993941a1fc5b2b6c0655c1b37883763
heechul <khc4190@naver.com>
2025-02-25 15:30:22 +0900

    변수 추출하기 리팩터링

--- src/main/java/study/refactoring/ch6/ExtractVariable.java
+++ src/main/java/study/refactoring/ch6/ExtractVariable.java
@@ -3,9 +3,10 @@ package study.refactoring.ch6;
 public class ExtractVariable {
 
 	public static double price(Order order) {
-		return order.getQuantity() * order.getItemPrice() -
-			Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05 +
-			Math.min(order.getQuantity() * order.getItemPrice() * 0.1, 100);
+		final double basePrice = order.getQuantity() * order.getItemPrice();
+		final double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+		final double shipping = Math.min(basePrice * 0.1, 100);
+		return basePrice - quantityDiscount + shipping;
 	}
 
 	public static class Order {
