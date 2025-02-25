0ca7982934e2ad8594d9c24f548e1251eeac5213
heechul <khc4190@naver.com>
2025-02-25 14:58:07 +0900

    함수 인라인하기 두번째 예시 리팩토링

--- src/main/java/study/refactoring/ch6/InlineMethod.java
+++ src/main/java/study/refactoring/ch6/InlineMethod.java
@@ -11,13 +11,9 @@ public class InlineMethod {
 
 	public static List<List<String>> reportLines(Customer aCustomer) {
 		final List<List<String>> lines = new ArrayList<>();
-		gatherCustomerData(lines, aCustomer);
+		lines.add(List.of("name", aCustomer.getName()));
+		lines.add(List.of("location", aCustomer.getLocation()));
 		return lines;
-	}
-
-	private static void gatherCustomerData(List<List<String>> out, Customer aCustomer) {
-		out.add(List.of("name", aCustomer.getName()));
-		out.add(List.of("location", aCustomer.getLocation()));
 	}
 
 	public static class Customer {
