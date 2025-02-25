c82efbaef77bbd02297574c83293579e7cceab2d
heechul <khc4190@naver.com>
2025-02-25 14:57:14 +0900

    함수 인라인하기 두번째 예시 테스트코드 작성

--- src/test/java/study/refactoring/ch6/InlineMethodTest.java
+++ src/test/java/study/refactoring/ch6/InlineMethodTest.java
@@ -2,6 +2,9 @@ package study.refactoring.ch6;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,10 +12,19 @@ class InlineMethodTest {
 
 	@ParameterizedTest
 	@CsvSource(value = {"6,2", "5,1"})
-	void testInlineMethod(int numberOfLateDeliveries, int result) {
+	void testRating(int numberOfLateDeliveries, int result) {
 		InlineMethod.Driver driver = new InlineMethod.Driver(numberOfLateDeliveries);
 
 		assertThat(InlineMethod.rating(driver)).isEqualTo(result);
+	}
+
+	@Test
+	void testReportLines() {
+		InlineMethod.Customer customer = new InlineMethod.Customer("홍길동", "서울시 강남구");
+		assertThat(InlineMethod.reportLines(customer)).containsExactly(
+			List.of("name", "홍길동"),
+			List.of("location", "서울시 강남구")
+		);
 	}
 
 }
