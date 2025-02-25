3da0454f6b34ade44977147ed24ca63d318fbd8f
heechul <khc4190@naver.com>
2025-02-25 16:12:56 +0900

    함수 선언 바꾸기 매개변수 추가하기 예시작성

--- src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
+++ src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
@@ -1,12 +1,27 @@
 package study.refactoring.ch6;
 
+import java.util.Queue;
+
 public class ChangeFunctionDeclaration {
 
 	private static double circum(double radius) {
 		return circumference(radius);
 	}
 
-	private static double circumference(double radius){
+	private static double circumference(double radius) {
 		return 2 * Math.PI * radius;
+	}
+
+	public static class Book {
+		private Queue<Customer> reservations;
+
+		public void addReservation(Customer customer) {
+			reservations.add(customer);
+		}
+
+	}
+
+	public static class Customer {
+
 	}
 }
