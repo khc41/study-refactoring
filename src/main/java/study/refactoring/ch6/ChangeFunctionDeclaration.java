9df6f9d0142f3568e37887b6ca09ce666888e83f
heechul <khc4190@naver.com>
2025-02-25 16:19:33 +0900

    함수 선언 바꾸기 정리

--- src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
+++ src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
@@ -4,20 +4,12 @@ import java.util.Queue;
 
 public class ChangeFunctionDeclaration {
 
-	private static double circum(double radius) {
-		return circumference(radius);
-	}
-
-	private static double circumference(double radius) {
+	public static double circumference(double radius) {
 		return 2 * Math.PI * radius;
 	}
 
 	public static class Book {
 		private Queue<Customer> reservations;
-
-		// public void addReservation(Customer customer) {
-		// 	zz_addReservation(customer, false);
-		// }
 
 		public void addReservation(Customer customer, boolean isPriority) {
 			reservations.add(customer);
