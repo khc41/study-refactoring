37acee5bf1ea7ca7bffdb92716b9675a83a46cfd
heechul <khc4190@naver.com>
2025-02-25 13:51:02 +0900

    함수 인라인하기 적용

--- src/main/java/study/refactoring/ch6/InlineMethod.java
+++ src/main/java/study/refactoring/ch6/InlineMethod.java
@@ -3,11 +3,7 @@ package study.refactoring.ch6;
 public class InlineMethod {
 
 	public static int rating(Driver aDriver) {
-		return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-	}
-
-	private static boolean moreThanFiveLateDeliveries(Driver aDriver) {
-		return aDriver.numberOfLateDeliveries > 5;
+		return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 	}
 
 	public static class Driver {
