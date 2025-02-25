c17c34857469fbbebee806a3fd1d5fb21251aa22
heechul <khc4190@naver.com>
2025-02-25 16:21:44 +0900

    함수 선언 바꾸기 매개변수를 속성으로 바꾸기

--- src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
+++ src/main/java/study/refactoring/ch6/ChangeFunctionDeclaration.java
@@ -1,11 +1,16 @@
 package study.refactoring.ch6;
 
+import java.util.List;
 import java.util.Queue;
 
 public class ChangeFunctionDeclaration {
 
 	public static double circumference(double radius) {
 		return 2 * Math.PI * radius;
+	}
+
+	public static boolean inNewEngland(Customer customer) {
+		return List.of("MA", "CT", "ME", "VT", "NH", "RI").contains(customer.getAddress().getState());
 	}
 
 	public static class Book {
@@ -18,6 +23,18 @@ public class ChangeFunctionDeclaration {
 	}
 
 	public static class Customer {
+		private Address address;
 
+		public Address getAddress() {
+			return address;
+		}
+	}
+
+	public static class Address {
+		private String state;
+
+		public String getState() {
+			return state;
+		}
 	}
 }
