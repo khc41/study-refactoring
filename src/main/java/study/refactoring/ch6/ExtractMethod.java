c35a394648f8ad9c576873b59cabf5055bdb2918
heechul <khc4190@naver.com>
2025-02-25 11:24:41 +0900

    출력부분 추출하기

--- src/main/java/study/refactoring/ch6/ExtractMethod.java
+++ src/main/java/study/refactoring/ch6/ExtractMethod.java
@@ -1,5 +1,6 @@
 package study.refactoring.ch6;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -8,9 +9,7 @@ public class ExtractMethod {
 	public static void printOwing(Invoice invoice) {
 		int outstanding = 0;
 
-		System.out.println("****************");
-		System.out.println("**** 고객 채무 ****");
-		System.out.println("****************");
+		printBanner();
 
 		// 미해결 채무(outstanding)를 계산한다.
 		for (Order o : invoice.getOrders()) {
@@ -21,10 +20,20 @@ public class ExtractMethod {
 		LocalDateTime today = LocalDateTime.now();
 		invoice.setDueDate(today.plusDays(30));
 
+		printDetails(invoice, outstanding);
+	}
+
+	private static void printDetails(Invoice invoice, int outstanding) {
 		// 세부 사항을 출력한다.
 		System.out.printf("고객명: %s%n", invoice.getCustomer());
 		System.out.printf("채무액: %s%n", outstanding);
 		System.out.printf("마감일: %s%n", invoice.getDueDate().toLocalDate().toString());
+	}
+
+	private static void printBanner() {
+		System.out.println("****************");
+		System.out.println("**** 고객 채무 ****");
+		System.out.println("****************");
 	}
 
 	public static class Invoice {
