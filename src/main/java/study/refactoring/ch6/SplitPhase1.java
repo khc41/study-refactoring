353a220532079a0ca477867d9cc547e056540213
heechul <khc4190@naver.com>
2025-02-27 15:16:00 +0900

    단계 쪼개기1 - 테스트를 위해 표준출력 상위 메서드로이동

--- src/main/java/study/refactoring/ch6/SplitPhase1.java
+++ src/main/java/study/refactoring/ch6/SplitPhase1.java
@@ -11,25 +11,25 @@ public class SplitPhase1 {
 
     public static void main(String[] args) {
         try {
-            run(args);
+            System.out.println(run(args));
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
     }
 
-    static void run(String[] args) throws IOException {
+    static long run(String[] args) throws IOException {
         if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
         String filename = args[args.length - 1];
         File input = Paths.get(filename).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
         if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-            System.out.println(Stream.of(orders)
+            return Stream.of(orders)
                     .filter(o -> "ready".equals(o.status))
-                    .count());
+                    .count();
         else
-            System.out.println(orders.length);
+            return orders.length;
     }
 
     public static class Order {
