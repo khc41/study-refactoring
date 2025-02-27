8cc0198b95d47bfdaaa4cf687fd66ff0a3721596
heechul <khc4190@naver.com>
2025-02-27 15:13:30 +0900

    단계 쪼개기1 - 예시코드 작성 및 핵심작업 코드 분리

---
+++ src/main/java/study/refactoring/ch6/SplitPhase1.java
@@ -0,0 +1,38 @@
+package study.refactoring.ch6;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class SplitPhase1 {
+
+    public static void main(String[] args) {
+        try {
+            run(args);
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
+
+    static void run(String[] args) throws IOException {
+        if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+        String filename = args[args.length - 1];
+        File input = Paths.get(filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+            System.out.println(Stream.of(orders)
+                    .filter(o -> "ready".equals(o.status))
+                    .count());
+        else
+            System.out.println(orders.length);
+    }
+
+    public static class Order {
+        public String status;
+    }
+}
